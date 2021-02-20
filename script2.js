b3b2e92b2c8dd8f8c1dfd9f88a54b63edf83c088
Tetiana Pavlenko <72665178+tpavlenko@users.noreply.github.com>
2021-02-20 14:17:24 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -4,13 +4,22 @@
 // sumTo(3) = 3 + 2 + 1 = 6
 
 function sumTo(n) {
-	if (n === 1) {
-		return 1;
-	} else {
-		return n + sumTo(n - 1);
-	}
+	return n != 1 ? n + sumTo(--n) : 1
 }
 
 var result = sumTo(5);
 
 console.log(result);
+
+
+// function sumTo(n) {
+// 	if (n === 1) {
+// 		return 1;
+// 	} else {
+// 		return n + sumTo(n - 1);
+// 	}
+// }
+
+// var result = sumTo(5);
+
+// console.log(result);
--- script3.js
+++ script3.js
@@ -1,13 +1,22 @@
 // Написать функция factorial(n) - которая при помощи рекурсии будет считать значение факториала числа n!
 
 function getFactorial(n) {
-	if (n === 1) {
-		return n;
-	} else {
-		return n * getFactorial(n - 1);
-	}
+	return n != 1 ? n * getFactorial(--n) : 1
 }
 
 var result = getFactorial(3);
 
 console.log(result);
+
+
+// function getFactorial(n) {
+// 	if (n === 1) {
+// 		return n;
+// 	} else {
+// 		return n * getFactorial(n - 1);
+// 	}
+// }
+
+// var result = getFactorial(3);
+
+// console.log(result);
